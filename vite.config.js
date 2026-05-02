9a940c2fb03c037609eb79861de1ffb90ef43e92
a7mad7asann <ahmadembaby333@gmail.com>
2026-05-02 22:20:43 +0300

    Change build output directory to build/

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'build',
     target: 'esnext',
     cssCodeSplit: true,
     rollupOptions: {
